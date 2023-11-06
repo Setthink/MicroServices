8c9de0daaf3737ac1170c7ace5838830371e5814
unknown <darkbluee2323@gmail.com>
2023-11-06 17:40:22 +0300

    Implemented Circuit Breaker

--- microservices/order-service/src/main/java/com/setthink/orderservice/controller/OrderController.java
+++ microservices/order-service/src/main/java/com/setthink/orderservice/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.setthink.orderservice.controller;
 
 import com.setthink.orderservice.dto.OrderRequest;
 import com.setthink.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,10 +16,15 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "placeOrderFallback")
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
         return "Order placed successfully";
     }
 
+
+    public String placeOrderFallback(OrderRequest orderRequest, RuntimeException e) {
+        return "Order placement failed";
+    }
 
 }
