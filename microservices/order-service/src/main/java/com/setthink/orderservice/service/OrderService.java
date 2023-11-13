4260e1dcd4a1d90b70651c913e65ea1723273eb3
unknown <darkbluee2323@gmail.com>
2023-11-13 16:22:12 +0300

    Dockerized the Application

--- microservices/order-service/src/main/java/com/setthink/orderservice/service/OrderService.java
+++ microservices/order-service/src/main/java/com/setthink/orderservice/service/OrderService.java
@@ -3,10 +3,12 @@ package com.setthink.orderservice.service;
 import com.setthink.orderservice.dto.InventoryResponse;
 import com.setthink.orderservice.dto.OrderLineItemsDto;
 import com.setthink.orderservice.dto.OrderRequest;
+import com.setthink.orderservice.event.OrderPlacedEvent;
 import com.setthink.orderservice.model.Order;
 import com.setthink.orderservice.model.OrderLineItems;
 import com.setthink.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -22,6 +24,7 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public String placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -52,6 +55,7 @@ public class OrderService {
 
         if(allProductsInStock){
             orderRepository.save(order);
+            kafkaTemplate.send("notificationTopic", new OrderPlacedEvent(order.getOrderNumber()));
             return "Order placed successfully";
         } else {
             throw new IllegalArgumentException("Product is not in stock, please try again later");
